501884a8b9fc1774d6585a300d3c27ad5b9c228b
Gerencia Virtual <gerencia.virtual@hotmail.com>
2019-09-26 11:22:26 -0500

    Se comienza a estructurar la ventana principal

---
+++ Scripts/Application.js
@@ -0,0 +1,68 @@
+﻿Ext.application({
+    name: 'MVC',
+
+    views: [
+        'Detail'
+    ],
+
+    controllers: [
+        'Detail'
+    ],
+
+    stores: [
+        'People'
+    ],
+
+    launch: function () {
+        Ext.create('Ext.container.Viewport', {
+            layout: 'border',
+            items: [{
+                region: 'north',
+                html: '<h1 class="x-panel-header">Interfaz Contable</h1>',
+                border: false,
+                margin: '0 0 5 0'
+            }, {
+                region: 'west',
+                collapsible: true,
+                title: 'Navegación',
+                width: 150
+                // could use a TreePanel or AccordionLayout for navigational items
+            }, {
+                region: 'south',
+                title: 'Información Adicional',
+                collapsible: true,
+                html: 'Este es un ejemplo con Sencha Ext JS',
+                split: true,
+                height: 100,
+                minHeight: 100
+            }, {
+                region: 'east',
+                title: 'Menú Lateral',
+                collapsible: true,
+                split: true,
+                width: 150
+            }, {
+                region: 'center',
+                xtype: 'tabpanel', // TabPanel itself has no title
+                activeTab: 0,      // First tab active by default
+                items: {
+                    title: 'Provisiones',
+                    html: 'Aquí irán las provisiones del mes actual'
+                }
+            }]
+        });
+
+        //Ext.create('Ext.container.Viewport', {
+        //    items: [
+        //        {
+        //            xtype: 'mvc-MasterView',
+        //            flex: 2
+        //        },
+        //        {
+        //            xtype: 'mvc-DetailView',
+        //            flex: 1
+        //        }
+        //    ]
+        //});
+    }
+});
---
+++ app/model/Person.js
@@ -0,0 +1,22 @@
+﻿Ext.define('MVC.model.Person', {
+    extend: 'Ext.data.Model',
+
+    fields: [
+        {
+            name: 'name',
+            type: 'string'
+        },
+        {
+            name: 'email',
+            type: 'string'
+        },
+        {
+            name: 'phone',
+            type: 'string'
+        },
+        {
+            name: 'id',
+            type: 'string'
+        }
+    ]
+});
